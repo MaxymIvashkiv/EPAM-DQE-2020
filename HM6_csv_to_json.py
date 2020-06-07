71158cb8e3aaee1df41b9865c6c2d38403807468
Maksym Ivashkiv <goliaf.maksimus@gmail.com>
2020-06-07 20:19:49 +0300

    Update HM6_csv_to_json.py

--- HM6_csv_to_json.py
+++ HM6_csv_to_json.py
@@ -1,12 +1,14 @@
-import argparse, csv, json
 
 parser = argparse.ArgumentParser(description="Convert csv file to json.")
 
 parser.add_argument("-csv", type=str, help="Path to csv",)
 
-parser.add_argument("-json", type=str, help="<Path and name of JSON file")
 
 arguments = parser.parse_args()
+
+path = os.path.split(arguments.json)[0]
+
+file_name = os.path.split(arguments.json)[1]
 
 data = {}
 
@@ -23,8 +25,15 @@ with open(arguments.csv, 'r') as opened_csv_file:
             data_row[field] = row[field]
         data[row["user_id"]] = data_row
 
-with open(arguments.json, 'w') as json_file:
-    # Записую у json файл лише value  для кожного user_id
-    for key in data:
-        json_file.write(json.dumps(data[key], indent=4))
-        json_file.write('\n')
+while True:
+    try:
+        with open(path + '\\' + file_name, 'w') as json_file:
+            # Записую у json файл лише value  для кожного user_id
+            for key in data:
+                json_file.write(json.dumps(data[key], indent=4))
+                json_file.write('\n')
+    except FileNotFoundError:
+        # створює нову папку якщо такої не існує
+        os.mkdir(path)
+    else:
+        break
